3a7f9b8ed4d21a2d1343f37b7bcf13c23a1eef66
frank66884 <2225338824@qq.com>
2026-02-25 13:03:09 +0800

    改为PWA应用

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,37 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import {VitePWA} from 'vite-plugin-pwa';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          name: '业忠计算器',
+          short_name: '业忠计算器',
+          start_url: '/',
+          display: 'standalone',
+          background_color: '#ffffff',
+          theme_color: '#111827',
+          icons: [
+            {
+              src: '/183.png',
+              sizes: '183x183',
+              type: 'image/png',
+            },
+          ],
+        },
+      }),
+    ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
